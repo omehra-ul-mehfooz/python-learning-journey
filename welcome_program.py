5b27ade976bade2639d5464f5307dc597ab29262
omehra <omehra1113@gmail.com>
2026-07-17 20:05:11 +0530

    Add Python introduction program
    ### Python Introduction Program
    
    An interactive beginner Python program where Python introduces itself, asks the user for their name and age, and displays a personalized message.
    
    **Concepts practiced:**
    
    * `print()`
    * Variables
    * Strings
    * `input()`
    * Converting input with `int()`
    * Displaying user-provided information

---
+++ welcome_program.py
@@ -0,0 +1,8 @@
+print('hello,there')
+intro= ' i am python, u can call me MR. python'
+print(intro)
+name= input(" what can i call u? ")
+print(' wow! nice name', name )
+age= int(input("and ur age?..if u dont mind.. "))
+print('nice to meet u',name)
+print('I HOPE U CREATE SOMETHING BIG N MEANIFUL ONE DAY!')
