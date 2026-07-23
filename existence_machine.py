284a1500db63f91b131071e2da0b575adbb6f8f0
omehra <omehra1113@gmail.com>
2026-07-23 19:04:56 +0530

    add existence machine
    Add an interactive Python program
     that converts the user's age into estimated days, hours, minutes,
    and seconds using variables, type conversion, and arithmetic calculations.

---
+++ existence_machine.py
@@ -0,0 +1,22 @@
+print("Welcome to calculate ur existence!")
+name= input("Tell me what's ur name?\n")
+print("hello",name," to cal. ur EXISTENCE..we neeed one more thing")
+print(' UR AGE!!')
+age= input(" so how many years have u been alive?\n")
+print("wow",age,"years of existence!")
+# lets cal. days 
+days= int(age)*365.25
+print("so u have been alive for",days,"days")
+#now hours
+hours= int(days*24)
+print(hours,"hours")
+#now minutes
+minutes= int(hours*60)
+print(minutes,"Minutes")
+#noww the last one SEC
+sec= int(minutes*60)
+print(sec,'Seconds...BROO!')
+print(' thats quite a long time to be alive..')
+print('ANDD GUESS WHATT... ')
+print('u have blinked about', days*364*24860*60,"times")
+print("yehhhhhhhhhhh i knwww ISN'T IT COOL!")
